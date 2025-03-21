330af73ae574e638f273d1d880875a080de0710b
Quentin Leveque <qleveque@hotmail.com>
2025-03-21 08:30:32 +0100

    Initial commit

---
+++ src/config.rs
@@ -0,0 +1,188 @@
+use std::{
+    collections::HashMap,
+    fs,
+    io::{BufRead, BufReader},
+    process::{Command, Stdio},
+};
+
+use crossterm::event::KeyCode;
+
+use crate::git::{FileStatus, GitFile, StagedStatus};
+
+pub type KeyBindings = HashMap<String, HashMap<String, String>>;
+
+pub struct Config {
+    pub scroll_off: usize,
+    pub git_exe: String,
+    pub bindings: KeyBindings,
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        Config {
+            scroll_off: 2,
+            git_exe: "git".to_string(),
+            bindings: HashMap::new(),
+        }
+    }
+}
+
+pub fn parse_gitrs_config() -> Config {
+    // TODO: better error handling
+    let path = std::env::var("HOME").unwrap() + "/.gitrsrc";
+    let result = fs::File::open(path);
+
+    let mut config: Config = Config::default();
+
+    if let Ok(file) = result {
+        let reader = BufReader::new(file);
+
+        for line in reader.lines() {
+            let line = line.unwrap();
+            let keyword = line
+                .split_once(' ')
+                .map(|(first, _)| first.to_string())
+                .unwrap_or(line.to_string());
+
+            match keyword.as_str() {
+                "map" => {
+                    let parts: Vec<&str> = line.splitn(4, ' ').collect();
+                    if parts.len() < 4 {
+                        continue;
+                    }
+                    let mode = parts[1].to_string();
+                    let key = parts[2].to_string();
+                    let command = parts[3].to_string();
+
+                    config
+                        .bindings
+                        .entry(mode)
+                        .or_insert_with(HashMap::new)
+                        .insert(key, command);
+                }
+                "set" => {
+                    let parts: Vec<&str> = line.splitn(3, ' ').collect();
+                    if parts.len() < 3 {
+                        continue;
+                    }
+                    let key = parts[1].to_string();
+                    let value = parts[2].to_string();
+                    match key.as_str() {
+                        "scrolloff" => {
+                            let number: Result<usize, _> = value.parse();
+                            if let Ok(so) = number {
+                                config.scroll_off = so
+                            }
+                        }
+                        "git" => {
+                            config.git_exe = value
+                        }
+                        _ => (),
+                    }
+                }
+                _ => (),
+            }
+        }
+    }
+
+    config
+}
+
+pub fn get_command_to_run(
+    config: &Config,
+    key: KeyCode,
+    fields: &mut Vec<(&str, bool)>,
+) -> Option<String> {
+    let KeyCode::Char(key_char) = key else {
+        return None;
+    };
+
+    fields.push(("global", true));
+    for field in fields {
+        if !field.1 {
+            continue;
+        }
+        if let Some(mode_map) = config.bindings.get(field.0) {
+            if let Some(command) = mode_map.get(&key_char.to_string()) {
+                return Some(command.clone());
+            }
+        }
+    }
+    None
+}
+
+pub fn get_status_command_to_run(
+    config: &Config,
+    key: KeyCode,
+    git_file: &GitFile,
+    staged_status: StagedStatus,
+) -> Option<String> {
+    let mut fields: Vec<(&str, bool)> = vec![
+        (
+            "unmerged",
+            staged_status == StagedStatus::Unstaged
+                && git_file.unstaged_status == FileStatus::Unmerged,
+        ),
+        (
+            "untracked",
+            staged_status == StagedStatus::Unstaged && git_file.unstaged_status == FileStatus::New,
+        ),
+        ("staged", staged_status == StagedStatus::Staged),
+        ("unstaged", staged_status == StagedStatus::Unstaged),
+        ("status", true),
+    ];
+    get_command_to_run(config, key, &mut fields)
+}
+
+pub fn get_show_command_to_run(config: &Config, key: KeyCode) -> Option<String> {
+    let mut fields: Vec<(&str, bool)> = vec![("show", true)];
+    get_command_to_run(config, key, &mut fields)
+}
+
+pub fn get_log_command_to_run(config: &Config, key: KeyCode) -> Option<String> {
+    let mut fields: Vec<(&str, bool)> = vec![("log", true)];
+    get_command_to_run(config, key, &mut fields)
+}
+pub fn get_blame_command_to_run(config: &Config, key: KeyCode) -> Option<String> {
+    let mut fields: Vec<(&str, bool)> = vec![("blame", true)];
+    get_command_to_run(config, key, &mut fields)
+}
+
+pub fn run_command(
+    mut command: String,
+    quit: &mut bool,
+    clear: &mut bool,
+    filename: Option<String>,
+    revision: Option<String>,
+) {
+    let command_type = command.chars().next().unwrap();
+    command = command[1..].to_string();
+
+    if let Some(file) = filename {
+        command = command.replace("%(file)", &file);
+    }
+    if let Some(rev) = revision {
+        command = command.replace("%(rev)", &rev);
+    }
+
+    let mut bash_proc = Command::new("bash");
+    let proc = bash_proc.args(["-c", &command]);
+
+    match command_type {
+        '@' => {
+            proc.stdout(Stdio::null())
+                .stderr(Stdio::null())
+                .status()
+                .expect("Failed to execute command");
+        }
+        '!' | '>' => {
+            let mut child = proc.spawn().expect("Failed to start git commit");
+            child.wait().expect("Failed to wait for git commit");
+            if command_type == '>' {
+                *quit = true;
+            }
+            *clear = true;
+        }
+        _ => (),
+    }
+}
